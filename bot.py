cc31fe4adbb5078a74752c335ca3fe8f8a9d2fea
DOAK515 <hohog8683@gmail.com>
2026-08-24 23:06:56 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+import time
+import requests
+import pandas as pd
+import yfinance as yf
+
+print("Bot started successfully...")
+
+def main():
+    while True:
+        print("Running bot check...")
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
