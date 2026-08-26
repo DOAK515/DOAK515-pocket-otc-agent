3a88260f6b7f1494e4917748a5411cd1c0625bbc
DOAK515 <hohog8683@gmail.com>
2026-08-26 23:13:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,13 @@
 import requests
 
-TELEGRAM_BOT_TOKEN = "8341287362:AAF0h06PMtcP5O2Y-sF34OffcN_zeLbIKNo"
-TELEGRAM_CHAT_ID = "-1003151787212"
+TOKEN = "8341287362:AAF0h06PMtcP5O2Y-sF34OffcN_zeLbIKNo"
+CHAT_ID = "-1003151787212"
 
-url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-payload = {
-    "chat_id": TELEGRAM_CHAT_ID, 
-    "text": "تجربة اتصال بوت أبو خالد", 
-    "parse_mode": "HTML"
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+data = {
+    "chat_id": CHAT_ID,
+    "text": "🚀 أهلاً بك يا أبو خالد! تم تشغيل البوت بنجاح تام عبر GitHub Actions وكل شي تمام."
 }
 
-response = requests.post(url, json=payload)
-print("Telegram API Response Code:", response.status_code)
-print("Telegram API Response Text:", response.text)
+response = requests.post(url, data=data)
+print("Response:", response.text)
