3a00dd6a0b601b88e2c6421e175768763b332535
DOAK515 <hohog8683@gmail.com>
2026-08-26 23:26:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import requests
 
-TOKEN = "8341287362:AAF0h06PMtcP5O2Y-sF34OffcN_zeLbIKNo"
+TOKEN = "8341287362:AAF0hO6PMtcP5O2Y-sF34OffcN_zeLbIKNo"
 CHAT_ID = "-1003151787212"
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 data = {
     "chat_id": CHAT_ID,
-    "text": "تجربة اتصال بوت أبو خالد"
+    "text": "🚀 أهلاً بك يا أبو خالد! تم ربط البوت وتشغيله بنجاح تام وكل شيء يعمل بانتظام."
 }
 
 print("جاري إرسال الطلب إلى تيليجرام...")
